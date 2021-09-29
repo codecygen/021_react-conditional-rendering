6ddd4a8835bf91562be9556ebb6daff74655c164
gingercook <vahitaras@gmail.com>
2021-09-28 22:01:46 -0400

    ternary operator added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import Login from './Login';
 
-function App() {
+let isLoggedIn = false;
+
+// For ternary operators you can use "null" as a condition
+// currentTime > 12 && <h1>Why are you still working?</h1>
+// && opearator exactly does the same thing with ternary operator question mark
+const App = () => {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {isLoggedIn ? <h1>Hello there...</h1> : <Login />}
     </div>
   );
 }
